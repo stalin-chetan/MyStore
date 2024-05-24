1b053a81b9900a4bb66751fae9c57772216de2a3
unknown <thechetanthapa@gmail.com>
2024-05-24 14:27:23 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { LoginSignup } from './Components/Loginsignup/LoginSignup';
+
+function App() {
+  return (
+    <div >
+      <LoginSignup/>
+
+    </div>
+  );
+}
+
+export default App;
